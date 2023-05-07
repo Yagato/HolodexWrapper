9494992d039d200846d3977106ac466b0a3a0280
Yagato <Yagatomon@gmail.com>
2023-05-07 04:29:47 -0600

    Refactor(Language): Changed ZH's value from zn to zh

--- src/main/java/constants/Language.java
+++ src/main/java/constants/Language.java
@@ -5,7 +5,7 @@ public class Language {
     public final static String EN = "en";
     public final static String JA = "ja";
     public final static String ES = "es";
-    public final static String ZH = "zn";
+    public final static String ZH = "zh";
     public final static String ID = "id";
     public final static String RU = "ru";
     public final static String KO = "ko";
